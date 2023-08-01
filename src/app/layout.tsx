78a4922efa818f0503d4a5bd8dab3c318f673bc1
a-t-klein <alxtkln@gmail.com>
2023-08-01 15:32:34 -0700

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'open art',
+  description: 'an open source tool for artists, galleries, and collectors to keep track of their art',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
